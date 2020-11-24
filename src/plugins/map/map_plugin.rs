032786cf51a89a3783d8fdcafe94b7ce73150bb7
Luc Bergeron <bergeronl@college-montreal.qc.ca>
2020-11-24 10:26:00 -0500

    Changed system name and saved the texture atlas in the MapSpriteHandle resource.

--- src/plugins/map/map_plugin.rs
+++ src/plugins/map/map_plugin.rs
@@ -11,6 +11,7 @@ pub struct MapPlugin;
 #[derive(Default)]
 pub struct MapSpriteHandles {
     handles: Vec<HandleUntyped>,
+    atlas_handle: Handle<TextureAtlas>,
     atlas_loaded: bool,
 }
 
@@ -19,15 +20,16 @@ impl Plugin for MapPlugin {
         app.init_resource::<MapSpriteHandles>()
             .init_resource::<Map>()
             .init_resource::<TileData>()
-            .add_startup_system(setup.system())
+            .add_startup_system(load_and_prepare_sprites)
             .add_system(render_map);
     }
 }
 
-fn setup(
+fn load_and_prepare_sprites(
     mut map_sprite_handles: ResMut<MapSpriteHandles>,
     asset_server: Res<AssetServer>,
     mut textures: ResMut<Assets<Texture>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>
 ) {
     map_sprite_handles.handles = asset_server.load_folder("map_tiles/").unwrap();
 
@@ -41,7 +43,9 @@ fn setup(
             texture_atlas_builder.add_texture(texture_id.clone_weak().typed::<Texture>(), &texture);
         }
 
-        let mut texture_atlas = texture_atlas_builder.finish(&mut textures).unwrap();
+        let texture_atlas = texture_atlas_builder.finish(&mut textures).unwrap();
+        map_sprite_handles.atlas_handle = texture_atlases.add(texture_atlas);
+                
         map_sprite_handles.atlas_loaded = true;
     }
 }
