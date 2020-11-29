e446d8100f9708ca311bce24c0d63bdfc97d3fd5
Luc Bergeron <psyluche@gmail.com>
2020-11-29 15:56:36 -0500

    Remove the sprite spawning to map_creator.

--- src/plugins/map/map_plugin.rs
+++ src/plugins/map/map_plugin.rs
@@ -10,9 +10,9 @@ pub struct MapPlugin;
 
 #[derive(Default)]
 pub struct MapSpriteHandles {
-    handles: Vec<HandleUntyped>,
+    pub handles: Vec<HandleUntyped>,
     pub atlas_handle: Handle<TextureAtlas>,
-    atlas_loaded: bool,
+    pub atlas_loaded: bool,
 }
 
 impl Plugin for MapPlugin {
@@ -20,33 +20,15 @@ impl Plugin for MapPlugin {
         app.init_resource::<MapSpriteHandles>()
             .init_resource::<Map>()
             .init_resource::<TileData>()
-            .add_startup_system(load_and_prepare_sprites)
+            .add_startup_system(load_sprites)
             .add_system(render_map);
     }
 }
 
-fn load_and_prepare_sprites(
+fn load_sprites(
     mut map_sprite_handles: ResMut<MapSpriteHandles>,
-    asset_server: Res<AssetServer>,
-    mut textures: ResMut<Assets<Texture>>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>
+    asset_server: Res<AssetServer>
 ) {
-    info!("load_and_prepare_sprites system");
+    info!("load_sprites system");
     map_sprite_handles.handles = asset_server.load_folder("map_tiles/").unwrap();
-
-    let mut texture_atlas_builder = TextureAtlasBuilder::default();
-
-    if let LoadState::Loaded =
-        asset_server.get_group_load_state(map_sprite_handles.handles.iter().map(|handle| handle.id))
-    {
-        for texture_id in map_sprite_handles.handles.iter() {
-            let texture = textures.get(texture_id).unwrap();
-            texture_atlas_builder.add_texture(texture_id.clone_weak().typed::<Texture>(), &texture);
-        }
-
-        let texture_atlas = texture_atlas_builder.finish(&mut textures).unwrap();
-        map_sprite_handles.atlas_handle = texture_atlases.add(texture_atlas);
-                
-        map_sprite_handles.atlas_loaded = true;
-    }
 }
