b3a1f557f0d47cac2fa5db3cdea857f8117a23a1
Luc Bergeron <psyluche@gmail.com>
2020-12-04 21:30:50 -0500

    WIP read mouse pos.

--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{render::camera::{
+    OrthographicProjection, WindowOrigin},
+    prelude::*
+};
 
 //use crate::plugins::map::map_creator::TILE_SIZE;
 
@@ -15,16 +18,34 @@ pub struct Player {
     pub direction: Direction,
 }
 
+pub struct CursorState {
+    cursor: EventReader<CursorMoved>,
+    camera_e: Entity,
+}
+
 pub fn spawn_player(
     commands: &mut Commands,
     asset_server: &Res<AssetServer>,
     materials: &mut ResMut<Assets<ColorMaterial>>,
 ) {
+    let camera = Camera2dBundle {
+            orthographic_projection: OrthographicProjection {
+                window_origin: WindowOrigin::BottomLeft,
+                ..Default::default()
+            },
+            ..Default::default()};
+
+    let e = commands.spawn(camera).current_entity().unwrap();
+    commands.insert_resource(CursorState {
+        cursor: Default::default(),
+        camera_e: e,
+    });
+
     let texture_handle = asset_server.load("unseen_horror_new.png");
     commands
         .spawn(SpriteBundle {
             material: materials.add(texture_handle.into()),
-            transform: Transform::from_translation(Vec3::new(1.0, 1.0, 5.0)),
+            transform: Transform::from_translation(Vec3::new(32.0, 1.0, 5.0)),
             ..Default::default()
         })
         .with(Player {
@@ -37,8 +58,24 @@ pub fn player_movement_system(
     time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
     windows: Res<Windows>,
+    mut state: ResMut<CursorState>,
+    ev_cursor: Res<Events<CursorMoved>>,
+    //q_camera: Query<&Transform>,
     mut query: Query<(&mut Player, &mut Transform)>,
 ) {
+    //Check world pos...
+    // let camera_transform = q_camera.get(state.camera_e).unwrap();
+
+    // for ev in state.cursor.iter(&ev_cursor) {
+    //     let wnd = windows.get(ev.id).unwrap();
+    //     let size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
+
+    //     let p = ev.position - size;
+
+    //     let world_pos = camera_transform.compute_matrix() * p.extend(0.0).extend(1.0);
+    //     println!("World coords: {}/{}", world_pos.x, world_pos.y);
+    // }
+
     for (mut player, mut transform) in query.iter_mut() {
         let mut direction = 0.0;
         let translation = &mut transform.translation;
